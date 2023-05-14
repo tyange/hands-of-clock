d94f2721f5240fdff82d635efd7aa8f1e5374ea5
tyange <usun16@gmail.com>
2023-05-14 09:48:05 +0900

    post item styling

--- src/components/post-list.svelte
+++ src/components/post-list.svelte
@@ -8,13 +8,15 @@
 	<section class="w-full m-auto sm:w-4/6 h-4/5">
 		<ul class="w-full h-full p-5">
 			{#each data.posts as post, index}
-				<li class="flex flex-col">
+				<li class="flex flex-col transition-all rounded-md hover:bg-gray-100">
 					<a
 						href={`/posts/${post.category}/${post.slug}`}
-						class="block w-full h-full p-3 transition-all hover:bg-gray-100"
+						class="flex flex-col w-full h-full gap-5 p-3"
 					>
-						<span class="text-2xl">{post.title}</span>
-						<p>{post.date}</p>
+						<div class="flex items-center justify-between w-full">
+							<span class="text-2xl">{post.title}</span>
+							<p class="text-xs">{post.date}</p>
+						</div>
 						<p>{post.description}</p>
 					</a>
 				</li>
